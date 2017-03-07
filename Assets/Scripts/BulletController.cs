0153198ea40ac1e96d17c752b20feef55c61c985
Jean Robert <jean.alves@ntconsult.com.br>
2017-03-07 13:08:42 -0300

    arrumando movimento das balas

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -12,11 +12,13 @@ public class BulletController : MonoBehaviour {
 
     protected Rigidbody2D myRigidbody;
     protected Vector3 startPosition;
+    protected Vector2 currentVelocity;
 
 	// Use this for initialization
 	protected virtual void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
         startPosition = gameObject.transform.position;
+        currentVelocity = speed * new Vector2(gameObject.transform.up.x, gameObject.transform.up.y);
     }
 	
     protected virtual void Update()
@@ -24,6 +26,8 @@ public class BulletController : MonoBehaviour {
         if (Vector3.Distance(startPosition, gameObject.transform.position) >= range)
         {
             OnRangeTraveled();
+        } else {
+            myRigidbody.velocity = currentVelocity;
         }
     }
 
@@ -35,7 +39,12 @@ public class BulletController : MonoBehaviour {
     // Update is called once per frame
     protected virtual void FixedUpdate()
     {
-        myRigidbody.AddForce(gameObject.transform.up * speed);
+
+
+        //myRigidbody.AddForce(gameObject.transform.up * speed);
+        //Debug.Log(gameObject.transform.up);
+
+        //myRigidbody.position = new Vector3(gameObject.transform.position.x + (horizontalStep * speed), gameObject.transform.position.y + (verticalStep * speed), gameObject.transform.position.z);
     }
 
     protected virtual void OnCollisionEnter2D(Collision2D other)
